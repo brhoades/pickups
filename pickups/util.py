170fc0751ba83d76be95aac1235ce0dd71e9ef3d
Billy Rhoades <billyarhoades@gmail.com>
2015-02-12 16:33:42 -0600

    No longer including hashes in channel names

--- pickups/util.py
+++ pickups/util.py
@@ -4,24 +4,20 @@ from hangups.ui.utils import get_conv_name
 import hashlib
 import re
 
-CONV_HASH_LEN = 7
-
-
 def conversation_to_channel(conv):
     """Return channel name for hangups.Conversation."""
     # Must be 50 characters max and not contain space or comma.
-    conv_hash = hashlib.sha1(conv.id_.encode()).hexdigest()
-    name = get_conv_name(conv).replace(',', '_').replace(' ', '')
-    return '#{}[{}]'.format(name[:50 - CONV_HASH_LEN - 3],
-                            conv_hash[:CONV_HASH_LEN])
+    name = get_conv_name(conv).replace(',', '').replace(' ', '_')
+    return '#{}'.format(name[:50 - 3])
 
 
 def channel_to_conversation(channel, conv_list):
     """Return hangups.Conversation for channel name."""
-    conv_hash = re.match(r'^.*\[([a-f0-9]+)\]$', channel)
-    conv_hash = conv_hash.group(1)
-    return {hashlib.sha1(conv.id_.encode()).hexdigest()[:CONV_HASH_LEN]: conv
-            for conv in conv_list.get_all()}[conv_hash]
+    for conv in conv_list.get_all():
+        if ''.join( [ '#', get_conv_name(conv).replace(',', '').replace( ' ', '_' ) ] ) == channel:
+            return conv 
+    else:
+        return None
 
 
 def get_nick(user):
