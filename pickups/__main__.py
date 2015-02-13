807f6dc1a4df06df0acc41fc1e4dbdb059f7e06d
Billy Rhoades <billyarhoades@gmail.com>
2015-02-12 19:14:02 -0600

    Credentials cache folder is created if missing now

--- pickups/__main__.py
+++ pickups/__main__.py
@@ -11,6 +11,8 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     logging.getLogger('hangups').setLevel(logging.WARNING)
     dirs = appdirs.AppDirs('hangups', 'hangups')
+    if not os.path.exists( dirs.user_cache_dir ):
+        os.mkdir( dirs.user_cache_dir )
     default_cookies_path = os.path.join(dirs.user_cache_dir, 'cookies.json')
     cookies = hangups.auth.get_auth_stdin(default_cookies_path)
     Server(cookies=cookies).run('localhost', 6667)
